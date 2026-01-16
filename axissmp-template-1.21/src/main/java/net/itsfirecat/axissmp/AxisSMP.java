c567735fd16b6f477d27f3d646227cf70d1e058f
cat <firecat9074@gmail.com>
2026-01-16 20:38:21 +0300

    Initial Commit

---
+++ axissmp-template-1.21/src/main/java/net/itsfirecat/axissmp/AxisSMP.java
@@ -0,0 +1,15 @@
+package net.itsfirecat.axissmp;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AxisSMP implements ModInitializer {
+	public static final String MOD_ID = "axissmp";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ axissmp-template-1.21/src/main/java/net/itsfirecat/axissmp/AxisSMPClient.java
@@ -0,0 +1,10 @@
+package net.itsfirecat.axissmp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AxisSMPClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
